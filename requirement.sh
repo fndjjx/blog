43d5618c3412a2e7438579d76e2e674fa05792be
ly <thunderocean@163.com>
2017-06-24 20:15:19 +0800

    Add requirement

---
+++ requirement.sh
@@ -0,0 +1,11 @@
+
+pip install flask-sqlalchemy
+pip install flask-login
+pip install flask-bootstrap
+pip install flask-wtform
+pip install flask-wtf
+pip install flask-pagedown
+pip install flask-markdown
+pip install flask-bleach
+pip install pymysql
+pip install python-markdown-math
