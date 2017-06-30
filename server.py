36c6eac367f7f22de4f449cd20025419a2b42526
ly <ly>
2017-06-30 21:18:51 +0800

    change port to 8080

--- server.py
+++ server.py
@@ -227,4 +227,4 @@ def load_user(user_id):
 db.event.listen(Post.text, 'set', Post.on_changed_text)
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
